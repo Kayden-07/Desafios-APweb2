448bf83fdd75cf43c312a35efba3e44741c53a66
gino mazzaglia <mazzagliagino@gmail.com>
2024-04-15 20:07:25 -0300

    cree carpeta desafios

---
+++ Clase2/assets/index.js
@@ -0,0 +1,16 @@
+console.log("te escribo desde el VS, jaj pt")
+
+let nombre = "gustavo"
+const nombre2 = "Lorena"
+
+let num1 = 4
+let num2 = 10
+
+let resultado = num1 + num2
+
+console.log("primer nombre " + nombre + "\nsegundo nombre " + nombre2)
+
+let nombreUsuario = prompt("ingresa tu nombre")
+
+console.log("Hola de nuevo " + nombreUsuario)
+
